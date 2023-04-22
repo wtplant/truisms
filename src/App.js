0c852180488b4c04dae53dc244c7fd6cf4949557
Bill Plant <billqn@Bills-MacBook-Pro.local>
2023-04-21 20:01:55 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { truisms } from "./Truisms"; // import the truisms array
+
+function getRandomTruisms() {
+  const randomIndices = new Set();
+
+  while (randomIndices.size < 3) {
+    randomIndices.add(Math.floor(Math.random() * truisms.length));
+  }
+
+  return Array.from(randomIndices).map((index) => truisms[index]);
+}
 
 function App() {
+  const [answer, setAnswer] = useState("");
+  const [output, setOutput] = useState(null);
+  const [questionVisible, setQuestionVisible] = useState(true); // add state for question visibility
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (answer.toLowerCase() === "no") {
+      setOutput("BE GONE");
+    } else {
+      setOutput(getRandomTruisms().join(" - "));
+    }
+    setQuestionVisible(false); // hide the question after submitting
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {questionVisible && ( // only show the question if questionVisible is true
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="answer">is it knowledge you seek </label>
+          <input
+            type="text"
+            id="answer"
+            value={answer}
+            onChange={(e) => setAnswer(e.target.value)}
+          />
+        </form>
+      )}
+      {output && <div>{output}</div>}
     </div>
   );
 }
